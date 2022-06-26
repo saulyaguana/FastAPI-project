eb47f12eaeeb8048526a11ff55997a3196cb48b4
Saúl Yaguana <saulyaguanacruz@gmail.com>
2022-06-26 11:30:09 -0500

    request_response_body

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
+#Python
+from typing import Optional
+
+#Pydantic
+from pydantic import BaseModel
+
+#FastApi
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
+
+#Models
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
+
 
 @app.get("/")
 def home():
     return {"hello": "David"}
+
+#Request and Responde Body
+
+@app.post("/person/new")
+def create_person(people: Person = Body(...)):
+    return people
