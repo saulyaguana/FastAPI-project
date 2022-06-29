0f63d0e2cc48c7cf8a0a10cfe2372e90fa06020a
Saúl Yaguana <saulyaguanacruz@gmail.com>
2022-06-29 16:36:40 -0500

    cookies_and_headers

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from pydantic import Field, EmailStr
 #FastApi
 from fastapi import FastAPI
 from fastapi import status
-from fastapi import Body, Query, Path, Form
+from fastapi import Body, Query, Path, Form, Header, Cookie
 
 app = FastAPI()
 
@@ -184,3 +184,31 @@ def login(
     password: str = Form(...)
 ):
     return LoginOut(username=username)
+
+
+#Cookies and Headers Parameters
+@app.post(
+    path="/contact",
+    status_code=status.HTTP_200_OK
+)
+def contact(
+    first_name: str = Form(
+        ...,
+        max_length=20,
+        min_length=1
+    ),
+    last_name: str = Form(
+        ...,
+        max_length=20,
+        min_length=1
+    ),
+    email: EmailStr = Form(...),
+    message: str = Form(
+        ...,
+        min_length=20,
+    ),
+    user_agent: Optional[str] = Header(default=None),
+    ads: Optional[str] = Cookie(default=None)
+):
+    return user_agent
+
