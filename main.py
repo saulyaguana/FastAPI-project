07f3095b02531ef2847a19392a2303034ebb2e61
Saúl Yaguana <saulyaguanacruz@gmail.com>
2022-06-29 22:01:00 -0500

    files

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from pydantic import Field, EmailStr
 #FastApi
 from fastapi import FastAPI
 from fastapi import status
-from fastapi import Body, Query, Path, Form, Header, Cookie
+from fastapi import Body, Query, Path, Form, Header, Cookie, UploadFile, File
 
 app = FastAPI()
 
@@ -211,4 +211,18 @@ def contact(
     ads: Optional[str] = Cookie(default=None)
 ):
     return user_agent
+
+
+#Files
+@app.post(
+    path="/post-image"
+)
+def post_image(
+    image: UploadFile = File(...)
+):
+    return {
+        "Filename": image.filename,
+        "Format": image.content_type,
+        "Size(kb)": round(len(image.file.read())/1024, ndigits=3)
+    }
 
